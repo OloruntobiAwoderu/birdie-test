c5c9356e3515e2a8de9d5407752452a295869e7d
Oloruntobi Awoderu <awoderuoloruntobi@gmail.com>
2021-12-07 09:22:38 +0100

    Chore: Realigned code

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -14,12 +14,10 @@ app.use(logger("dev"));
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api/v1/events", eventRoute);
 
 const buildPath = path.join(__dirname, '../../../frontend', 'build');
-console.log(buildPath);
 app.use(express.static(buildPath));
-
-app.use("/api/v1/events", eventRoute);
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname+'../../../frontend/build/index.html'));
