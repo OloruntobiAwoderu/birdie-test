5bb5d6da58dc780e5f3095c2464ee66459d595b4
Oloruntobi Awoderu <awoderuoloruntobi@gmail.com>
2021-12-07 01:57:09 +0100

    Chore: Added build path

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cors = require('cors')
+const path = require("path");
 
 const eventRoute = require("./events");
 
@@ -14,7 +15,15 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const buildPath = path.join(__dirname, '../../../frontend', 'build');
+console.log(buildPath);
+app.use(express.static(buildPath));
+
 app.use("/api/v1/events", eventRoute);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'../../../frontend/build/index.html'));
+  });
 
 app.use(function errors(err, req, res, next) {
 	return res.status(500).json({ err });
