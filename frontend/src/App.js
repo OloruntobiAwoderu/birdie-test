288f3910fbd93032e93b150e66cd1522456ff534
Oloruntobi Awoderu <awoderuoloruntobi@gmail.com>
2021-12-07 09:15:53 +0100

    Chore: Added Heroku config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
 
 	const fetchEvents = async () => {
 		const res = await fetch(
-			`http://localhost:8000/api/v1/events/date?date=:${date}`
+			`/api/v1/events/date?date=:${date}`
 		);
 		const data = await res.json();
 		return data;
