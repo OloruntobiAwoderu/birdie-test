2fa3423239d590b2d654cd440a82e1fca1e36b0f
Oloruntobi Awoderu <awoderuoloruntobi@gmail.com>
2021-12-07 10:19:28 +0100

    Chore: Uninstalled helmet

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cors = require('cors')
@@ -9,7 +8,6 @@ const eventRoute = require("./events");
 
 const app = express();
 
-app.use(helmet());
 app.use(logger("dev"));
 app.use(cors())
 app.use(bodyParser.json());
